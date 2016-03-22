d618c3e6b67932df5596560cb67fc17e09756700
Milos Vasic <mvasic@vermilion-code.com>
2016-03-22 21:16:11 +0100

    First commit.

---
+++ src/com/weather_report/Main.java
@@ -0,0 +1,11 @@
+package com.weather_report;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String argument : args) {
+            System.out.println("Argument: " + argument);
+        }
+    }
+
+}
