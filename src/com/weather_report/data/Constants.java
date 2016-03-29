382914b2c2990096ca3800683e9e1ec9e04ece4c
Milos Vasic <mvasic@vermilion-code.com>
2016-03-29 21:54:51 +0200

    Retrofit dependancy added.

--- src/com/weather_report/data/Constants.java
+++ src/com/weather_report/data/Constants.java
@@ -5,7 +5,7 @@ package com.weather_report.data;
  */
 public class Constants {
 
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
     public static final long CODE_VERSION = 1;
     public static final String PARAMETER_LEADING_SIGN = "--";
 
