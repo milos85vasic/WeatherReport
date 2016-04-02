e746189bd037aed4c063b362ea0ee48ce5e25dab
Milos Vasic <office@milosvasic.net>
2016-04-02 19:03:47 +0200

    TODOs.

--- src/com/weather_report/networking/HttpManager.java
+++ src/com/weather_report/networking/HttpManager.java
@@ -10,7 +10,7 @@ public enum HttpManager implements Pull<String, String> {
     GET {
         @Override
         public String retrieve(String... urlParams) throws IllegalStateException {
-
+            // TODO: Finish this.
             return null;
         }
     },
@@ -18,25 +18,9 @@ public enum HttpManager implements Pull<String, String> {
     POST {
         @Override
         public String retrieve(String... params) throws IllegalStateException {
-
+            // TODO: To be implemented.
             return null;
         }
     }
-
-
-//    StringBuilder result = new StringBuilder();
-//    URL url = new URL(urlToRead);
-//    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//    conn.setRequestMethod("GET");
-//    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//    String line;
-//    while ((line = rd.readLine()) != null) {
-//        result.append(line);
-//    }
-//    rd.close();
-//    return result.toString();
-
-
-
 
 }
